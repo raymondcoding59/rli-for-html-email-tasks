532d94c187f509f0363747e9688855d9f3d6b8c8
Raymond <raymondcoding59@gmail.com>
2026-02-03 06:40:49 +0100

    further layout analysis.

---
+++ 2026-02/02/layout-analysis-2.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+import numpy as np
+from pathlib import Path
+import time
+import json
+
+
+# Define the base directory
+BASE_DIR = Path(__file__).resolve().parent
+
+# Start timer and show running message
+start_time = time.time()
+print("running...")
+
+
+# Load and display the original email design 
+original_design = cv.imread(BASE_DIR / "design-2.png")
+# cv.imshow('Original Design', original_design)
+
+
+# determine the hex background colour of the email
+hex_background_color = '#{:02x}{:02x}{:02x}'.format(original_design[0,0,2], original_design[0,0,1], original_design[0,0,0])
+print("Hex Background Color:", hex_background_color)
+
+
+# determine the width of the email apart from the background spaces
+content_width = 0
+for col in range(original_design.shape[1]):
+    if not all(original_design[row, col][0] == original_design[0, 0][0] and
+               original_design[row, col][1] == original_design[0, 0][1] and
+               original_design[row, col][2] == original_design[0, 0][2]
+               for row in range(original_design.shape[0])):
+        content_width += 1
+
+print("Content Width:", content_width)
+
+#determine the height of the padding at the top of the email befor the content starts
+top_padding_height = 0
+for row in range(original_design.shape[0]):
+    if all(original_design[row, col][0] == original_design[0, 0][0] and
+           original_design[row, col][1] == original_design[0, 0][1] and
+           original_design[row, col][2] == original_design[0, 0][2]
+           for col in range(original_design.shape[1])):
+        top_padding_height += 1
+    else:
+        break
+
+print("Top Padding Height:", top_padding_height)
+    
+# End timer and show done message
+end_time = time.time()
+print("done in", round(end_time - start_time, 2), "seconds")
+cv.waitKey(0)
